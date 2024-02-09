fd75e7e562ba4155a411af7998a2160d6dce5f98
Rémi Tran <tranremi@live.fr>
2024-02-09 11:02:29 +0100

    Example > clarification

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -27,7 +27,7 @@ const uni = new THREE.Uniform(texture);
 GlyphShader.uniforms.map = uni;
 const material = new THREE.RawShaderMaterial(GlyphShader);
 const geometry = new GlyphGeometry({
-  text: 'LO. -VÉ LOVE',
+  text: 'LO-VÉ.',
   font,
 });
 const mesh = new THREE.Mesh(geometry, material);
